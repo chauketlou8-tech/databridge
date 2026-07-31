5a66a8ac5d861462540a046a7dae7a8b01bc060d
Chauke Tlou <chauketlou8@gmail.com>
2026-07-31 19:59:04 +0200

    add mongodb driver

---
+++ src/drivers/mongodb/MongoDriver.ts
@@ -0,0 +1,50 @@
+import Driver from "../Driver";
+import type { Config } from "../../types/config";
+import {ConnectionError, DriverError} from "../../exceptions"
+
+import { MongoClient, Db } from "mongodb";
+
+export class MongoDriver extends Driver {
+    private client: MongoClient | null;
+    private db: Db | null;
+
+    constructor(config: Config) {
+        super(config);
+        this.client = null;
+        this.db = null;
+    }
+
+    // example url: mongodb+srv://username:Password@cluster_name.h6kymkq.mongodb.net/BD_name?retryWrites=true&w=majority
+
+    public async connect(): Promise<void> {
+        if (!this.config.url) {
+            throw new DriverError("The connection URL is missing or empty", "D024");
+        }
+
+        try {
+            this.client = new MongoClient(this.config.url);
+            await this.client.connect();
+
+            const match = this.config.url.match(/^mongodb(?:\+srv)?:\/\/[^/]+\/([^?]+)/);
+            const dbName = match?.[1];
+
+            this.db = this.client.db(dbName);
+        }
+        catch (error) {
+            throw new ConnectionError(`Failed to connect to MongoDB: ${error instanceof Error ? error.message : String(error)}`);
+        }
+    }
+
+    public async disconnect(): Promise<void> {
+        await this.client?.close();
+        console.log("disconnected...");
+    }
+
+    public async query(sql: string): Promise<any> {
+        if (!this.db) {
+            throw new ConnectionError("Not connected to database", "D015");
+        }
+        // this one is complicated will add it later
+        //return await this.pool.query(sql);
+    }
+}
