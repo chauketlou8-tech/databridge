5e1b2bcf982b9388ede0459bd9dae8f9f5fc0953
Chauke Tlou <chauketlou8@gmail.com>
2026-08-02 01:45:15 +0200

    add manual test for model

---
+++ tests/manual/Model.test.ts
@@ -0,0 +1,21 @@
+import { DataBridge, Schema } from "../../src";
+async function run() {
+    const db = await DataBridge.connect({
+        provider: "postgres",
+        url: "postgresql://postgres:password@localhost:port/databaseName"
+    });
+    const User = await db.model(
+        "User",
+        new Schema({
+            name: String,
+            email: String,
+            age: Number
+        }));
+    // await User.create({
+    //     name: "John",
+    //     email: "john@example.com",
+    //     age: 21
+    // });
+}
+
+void run();
